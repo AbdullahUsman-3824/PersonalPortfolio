5e809c0f28a139f240cb554c7f8b87478be17e5b
AbdullahUsman-3824 <abdullaasheikh3824@gmail.com>
2024-08-21 13:01:41 +0500

    Update GitHub Actions to avoid deprecation warnings

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/PersonalPortfolio/",
-  publicDir: "public",
+  publicDir: "public", // Ensure this is set
+  build: {
+    outDir: "dist", // Output directory
+  },
 });
